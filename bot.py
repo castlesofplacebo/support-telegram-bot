ea030ad6591f1c0c4c640c7a0f1ad0c6ea8165d8
Anna <castlesofplacebo@gmail.com>
2021-10-08 09:02:34 +0300

    chore: searching (by id) added

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
+import asyncio
+import json
+
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
 from aiogram.types.message import ContentType
-from tortoise import Tortoise, fields, run_async
+from aiogram.utils import executor
 
 from config import TOKEN
 from database.db_init import storage, run_db
 from database.models import MessageInfo
-
-import asyncio
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=storage)
@@ -26,13 +26,29 @@ async def get_messages(message: types.Message):
     print(await MessageInfo.all().values())
 
 
+@dp.message_handler(commands=['search'])
+async def search_by_username(message: types.Message):
+    argument = message.get_args()
+    message_list = await MessageInfo.filter(username=argument).values("data")
+    for i in message_list:
+        # to chat
+        str_message = i.get('data')
+        dict_message = json.loads(str_message)
+        new_msg = types.Message(**dict_message)
+        await new_msg.send_copy(message.chat.id)
+
+        # to console
+        # print(i.get('data'))
+
+
 @dp.message_handler(content_types=ContentType.ANY)
 async def send_message(message: types.Message):
-    await MessageInfo(data=str(message)).save()
+    await MessageInfo(username=message.from_user.id, data=str(message)).save()
     await message.reply("Ваше сообщение отправлено администратору")
 
 
 if __name__ == '__main__':
+    print("Started")
     loop = asyncio.get_event_loop()
     loop.create_task(run_db())
     executor.start_polling(dp)
--- database/models.py
+++ database/models.py
@@ -4,6 +4,7 @@ from tortoise import fields
 
 class MessageInfo(Model):
     id = fields.IntField(pk=True)
+    username = fields.TextField()
     data = fields.TextField()
 
     class Meta:
